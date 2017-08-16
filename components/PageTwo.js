12c1c26989f3b4b8dc42653d391e94c221274ad8
Rod Nolan <rodnolan@gmail.com>
2017-08-16 10:33:18 -0400

    initialize Account Kit and check for an existing session

--- components/PageTwo.js
+++ components/PageTwo.js
@@ -5,8 +5,37 @@ import {
   View,
   Button
 } from 'react-native';
+import AccountKit from 'react-native-facebook-account-kit';
 
 export default class HomeScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      authToken: null,
+      loggedAccount: null
+    };
+  }
+
+  componentWillMount() {
+    AccountKit.configure({
+      countryWhitelist: ["CA", "US"],
+      defaultCountry: "CA"
+    });
+
+    AccountKit.getCurrentAccessToken().then((token) => {
+      if (token) {
+        AccountKit.getCurrentAccount().then((account) => {
+          this.setState({ authToken: token, loggedAccount: account });
+          console.log('already logged in');
+        })
+      } else {
+        console.log('No account logged in')
+      }
+    })
+    .catch((e) => console.log('Access token request failed', e))
+  }
+
   render() {
     return (
       <View style={styles.container}>
